ae8c974a3b31b6bd7ccb6ca237d0fa40fc67816d
Alex Koepke <alexkoepke@users.noreply.github.com>
2017-02-12 18:21:12 -0500

    create app gallery

---
+++ marketing/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>w00t!</h1>')
+
+def history(request):
+
+    context = {
+        'dob': 'August 31, 1989',
+        'full_name': 'Alex Koepke',
+        'first_name': 'Alex',
+    }
+
+    return render(request, 'history.html', context)
+
+def contact(request):
+
+    context = {
+        'phone_number': '(513) 236-7281',
+        'email': 'alex.koepke@gmail.com',
+    }
+
+    return render(request, 'contact.html', context)
